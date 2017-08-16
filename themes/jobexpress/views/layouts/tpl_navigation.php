36b544bcda46f9e9fc087cd60910a65898a3defb
Mugi Rachmat <infomugi@gmail.com>
2017-08-16 17:42:33 +0700

    Rev Menu & Theme

--- themes/jobexpress/views/layouts/tpl_navigation.php
+++ themes/jobexpress/views/layouts/tpl_navigation.php
@@ -31,11 +31,7 @@
          'encodeLabel'=>false,
          'items'=>array(
           array('label'=>'Home', 'url'=>array('/site/index'),'visible'=>YII::app()->user->getLevel()!=2),
-  								// array('label'=>'Profil', 'url'=>array('/site/about'),'visible'=>YII::app()->user->isGuest),
-  								// array('label'=>'Visi & Misi', 'url'=>array('/site/visimisi'),'visible'=>YII::app()->user->isGuest),
-  								// array('label'=>'Lowongan Kerja', 'url'=>array('/lowongan/index'),'visible'=>YII::app()->user->isGuest),
           array('label'=>'Register','url'=>array('/site/register'),'visible'=>yii::app()->user->isGuest),
-
           array('label'=>'Data Diri','url'=>array('/pelamar/profile'),'visible'=>yii::app()->user->getLevel()==2),
           array('label'=>'Pengajuan Lamaran','url'=>array('/lowongan/terbaru'),'visible'=>yii::app()->user->getLevel()==2),
           array('label'=>'Pengumuman','url'=>array('/filelamaran/history'),'visible'=>yii::app()->user->getLevel()==2),
@@ -44,8 +40,6 @@
            'items'=>array(
             array('label'=>'Kelola Data Akun','url'=>array('/user/admin')),
             array('label'=>'Kelola Data Pelamar','url'=>array('/pelamar/admin')),
-                      // array('label'=>'Pelamar Diterima','url'=>array('/pelamar/index')),
-                      // array('label'=>'Pelamar Ditolak','url'=>array('/pelamar/index')),
             ),'visible'=>Yii::app()->user->getLevel()==1), 
 
           array('label'=>'Job Order <span class="caret"></span>', 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
@@ -62,32 +56,6 @@
             array('label'=>'Provinsi','url'=>array('/provinsi/admin')),
             array('label'=>'Kota','url'=>array('/kota/admin')),
             ),'visible'=>Yii::app()->user->getLevel()==1),                                      
-
-                  // array('label'=>'Pengajuan <span class="caret"></span>', 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
-                  //   'items'=>array(
-                  //     array('label'=>'Tenaga Kerja','url'=>array('/pengajuan/admin')),
-                  //     array('label'=>'Pengajuan Karyawan','url'=>array('/pengajuan/pengajuan','id'=>Yii::app()->user->id),'visible'=>yii::app()->user->getLevel()==1),
-                  //     ),'visible'=>Yii::app()->user->getLevel()==1),
-
-                  // array('label'=>'Pengumuman <span class="caret"></span>', 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
-                  //   'items'=>array(
-                  //     array('label'=>'Pengumuman Test Ke-1','url'=>array('/pelamar/PengumumanTest1')),
-                  //     array('label'=>'Pengumuman Test Ke-2','url'=>array('/pelamar/PengumumanTest2')),
-                  //     array('label'=>'Pengumuman Test Ke-3','url'=>array('/pelamar/PengumumanTest3')),
-                  //     ),'visible'=>Yii::app()->user->getLevel()==1),
-
-                  // array('label'=>'Laporan <span class="caret"></span>', 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
-                  //   'items'=>array(
-                  //     array('label'=>'Laporan Test Ke-1','url'=>array('/pelamar/LaporanTest1')),
-                  //     array('label'=>'Laporan Test Ke-2','url'=>array('/pelamar/LaporanTest2')),
-                  //     array('label'=>'Laporan Test Ke-3','url'=>array('/pelamar/LaporanTest3')),
-                  //     ),'visible'=>Yii::app()->user->getLevel()==1),
-
-                  // array('label'=>'Help <span class="caret"></span>', 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
-                  //   'items'=>array(
-                  //     array('label'=>'Manual Book Admin','url'=>array('/docx/help' )),
-                  //     array('label'=>'Manual Book User','url'=>array('/user/admin')), 
-                  //     ),'visible'=>Yii::app()->user->getLevel()==1),
 
           array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
           array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
